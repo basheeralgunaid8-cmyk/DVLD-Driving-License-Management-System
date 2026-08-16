ee5dad1ef3562a28d19739cd0d3e51f6478060cb
basheer <basheeralgunaid8@gmail.com>
2026-08-16 16:22:33 +0800

    Add DTO project

---
+++ DTO Project/UserDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_Project
+{
+    public class UserDTO
+    {
+        public int UserID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public int PersonID { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string FullName { get; set; }
+    }
+}
